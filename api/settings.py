af5aee6cc835aa3853c59c749d42a2c46b443425
Alexander R <tcristian_15.go@hotmail.com>
2020-11-06 01:28:28 -0500

    prod t1

--- api/settings.py
+++ api/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+STATIC_ROOT = BASE_DIR.child('staticfiles')
 
 # add this block below MIDDLEWARE
 CORS_ORIGIN_WHITELIST = (
